7790b8a601d6820102f74a55da99f92b18202827
sr2328 <yadavsachin9311@gmail.com>
2025-06-08 20:01:12 +0530

    Updated gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/blogistan-hub/', // 👈 Add this for GitHub Pages deployment
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    exclude: ['lucide-react']
+  }
 });
